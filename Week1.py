a4821cf58dbeb113b1128ebf89410b028674bd58
edmondweb <edmondbonventure@gmail.com>
2025-08-19 13:26:34 +0300

    Python assignments

---
+++ Week1.py
@@ -0,0 +1,9 @@
+# A Simple Calculator
+
+x = int(input("Enter Num1: "))
+y = int(input("Enter Num2: "))
+
+print("The sum is:", x + y)
+print("The difference is:", x - y)
+print("The product is:", x * y)
+print("The quotient is:", x / y)
---
+++ Week2.py
@@ -0,0 +1,24 @@
+# Creating an empty list
+
+my_list = []
+
+# Appending elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Inserting an element at a specific index
+my_list.insert(2, 15)
+
+# Remove an element from the list
+my_list.pop(-1)
+
+# Sorting the list in ascending order
+my_list.sort()
+
+# Find the index of an element
+index = my_list.index(30)
+
+# Print the index of the element
+print("Index of element 30:", index)
